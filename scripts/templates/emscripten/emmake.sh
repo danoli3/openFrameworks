f5a8e74faba1beec891ecf071fa68536164b04bf
Dan Rosser <danoli3@gmail.com>
2026-04-08 19:11:42 +1000

    Emscripten Template bash

--- scripts/templates/emscripten/emmake.sh
+++ scripts/templates/emscripten/emmake.sh
@@ -1,5 +1,17 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SCRIPT_DIR
 
-emmake make
+# emconfigure ./configure
+emcc --clear-cache
+emcc --version
+make clean
+rm -rf obj
+rm -rf ../../../libs/openFrameworksCompiled/lib/emscripten
+rm -rf ../../../addons/obj/emscripten
+EMCC_DEBUG=0 emmake make clean
+EMCC_DEBUG=0 emmake make
+
+echo "Generating CSP..."
+# bash ../../../libs/openFrameworksCompiled/project/emscripten/generate_csp.sh
--- scripts/templates/emscripten/emmake_debug.sh
+++ scripts/templates/emscripten/emmake_debug.sh
@@ -3,9 +3,17 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SCRIPT_DIR
 
-emconfigure ./configure
+# emconfigure ./configure
+emcc --clear-cache
 emcc --version
-emmake make -j Debug
+make clean
 
+rm -rf obj
+rm -rf ../../../libs/openFrameworksCompiled/lib/emscripten
+rm -rf ../../../addons/obj/emscripten
+EMCC_DEBUG=1 emmake make clean
+EMCC_DEBUG=1 emmake make
 
-emrun bin/em/emscripten_debug --browser chrome  --enable-unsafe-webgpu
+echo "Generating CSP..."
+# bash ../../../libs/openFrameworksCompiled/project/emscripten/generate_csp.sh
+#emrun bin/em/emscripten_debug --browser chrome  --enable-unsafe-webgpu
